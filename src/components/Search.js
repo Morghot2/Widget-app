97005fe6c3be5464fae8eaa3f84fdfe7f76455e0
Morghot2 <maximilian.jaron@gmail.com>
2022-01-16 23:43:33 +0100

    Executing request with useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-    const [term, setTerm] = useState('')
-    console.log("I will be every rerender")
-    useEffect(() => {
-      console.log('I only run once')
-    }, [])
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get('http://en.wikipedia.org/w/api.php', {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      
+    }; 
+    search();
+  }, [term]);
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Enter searched term</label>
-          <input value={term} onChange={e => setTerm(e.target.value)} className="input"></input>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          ></input>
         </div>
       </div>
     </div>
