073d5155cc55e85eb2121e4ae0fd00fe734f8cad
Morghot2 <maximilian.jaron@gmail.com>
2022-01-18 16:33:22 +0100

    Scaffolding the Dropdown

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from 'react'
-import Accordion from './components/Accordion'
-import Search from './components/Search'
-
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front-end Javascript framework.',
-    },
-    {
-        title: 'Why ure React',
-        content: 'React is favourite Js library among engineers'
-    },
-    {
-        title: 'How do you use react?',
-        content: 'You use React by creating components.'
-    }
-]
+  {
+    title: "What is React?",
+    content: "React is a front-end Javascript framework.",
+  },
+  {
+    title: "Why ure React",
+    content: "React is favourite Js library among engineers",
+  },
+  {
+    title: "How do you use react?",
+    content: "You use React by creating components.",
+  },
+];
+const options = [
+  {
+    label: "The color Red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "Green",
+  },
+  {
+    label: "A shade of Blue",
+    value: "Blue",
+  },
+];
 
 export default () => {
-    return <div><Search /></div>
-}
+  return (
+    <div>
+      <Dropdown options={options}/>
+    </div>
+  );
+};
