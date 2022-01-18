6a5e9913b39078fa2616ab81b4195b8a41258d5a
Morghot2 <maximilian.jaron@gmail.com>
2022-01-18 23:28:42 +0100

    Filtering option list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,12 +2,18 @@ import React from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null
+    }
     return (
-      <div key={option.value} className="item" onClick={() => {
-          onSelectedChange(option)
-      }}>
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+          onSelectedChange(option);
+        }}
+      >
         {option.label}
-        
       </div>
     );
   });
