fe1595ae378febfa1a7321da5f2be564b12ed09b
Morghot2 <maximilian.jaron@gmail.com>
2022-01-20 12:55:36 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./components/Route";
+import Header from "./components/Header";
 const items = [
   {
     title: "What is React?",
@@ -33,36 +34,13 @@ const options = [
   },
 ];
 
-const showAccordion = () => {
-  if (window.location.pathname === "/") {
-    return <Accordion items={items} />;
-  }
-};
 
-const showList = () => {
-  if (window.location.pathname === "/list") {
-    return <Search />;
-  }
-};
 
-const showDropdown = () => {
-  if (window.location.pathname === "/dropdown") {
-    return <Dropdown />;
-  }
-};
-
-const showTranslate = () => {
-  if (window.location.pathname === "/translate") {
-    return <Translate />;
-  }
-};
-const showComponent = (route, component) => {
-  return window.location.pathname === route ? component : null;
-};
 export default () => {
-  const [selected, setSelected] = useState(options[0])
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
@@ -70,7 +48,12 @@ export default () => {
         <Search />
       </Route>
       <Route path="/dropdown">
-        <Dropdown label='Select a color' options={options} selected={selected} onSelectedChange={setSelected}/>
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
       </Route>
       <Route path="/translate">
         <Translate />
